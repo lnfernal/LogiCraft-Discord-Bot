15659ed21a55b5fcc9425ebf8bb8c3ae4b5cdf65
mariod8 <mariodavo.20@gmail.com>
2021-03-31 16:11:49 +0200

    sisp

--- troll.js
+++ troll.js
@@ -7,9 +7,9 @@ var emojis = [
 module.exports = (client) => {
     client.on("message", async (message) => {
         if(message.member.id === "458738156695584770" && message.content.toLowerCase().includes(["sisplau","sisp","sispi"])){
-            message.reply("sisplau😜")
+            message.channel.send(`${message.member.displayName}, deja a sisplau. aviso. desplegaré mis armas`)
         }
-        else if(message.member.id === "458738156695584770"){
+        else if(message.member.id === "355636164897669122"){
             for(i = 0; i < 10; i++){
                 message.react(emojis[Math.floor(Math.random() * emojis.length)])
             }
