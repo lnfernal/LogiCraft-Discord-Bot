262fe293bf74abe75b985c9b510c1e0a37a0acb4
mariod8 <mariodavo.20@gmail.com>
2021-03-31 16:19:40 +0200

    lol

--- troll.js
+++ troll.js
@@ -6,10 +6,10 @@ var emojis = [
 //siber
 module.exports = (client) => {
     client.on("message", async (message) => {
-        if(message.member.id === "458738156695584770" && message.content.toLowerCase().includes(["sisplau" || "sisp" || "sispi"])){
+        if(message.member.id === "458738156695584770" && message.content.toLowerCase().includes("sispi")){
             message.channel.send(`${message.member.displayName}, deja a sisplau. aviso. desplegaré mis armas`)
         }
-        else if(message.member.id === "355636164897669122"){
+        else if(message.member.id === "null"){
             for(i = 0; i < 10; i++){
                 message.react(emojis[Math.floor(Math.random() * emojis.length)])
             }
