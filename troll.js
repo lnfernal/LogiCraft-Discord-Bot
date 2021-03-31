54236c2c9d0c2665cc16ec024e3f1144b07585c3
mariod8 <mariodavo.20@gmail.com>
2021-03-31 16:03:42 +0200

    changes

--- troll.js
+++ troll.js
@@ -6,7 +6,10 @@ var emojis = [
 //siber
 module.exports = (client) => {
     client.on("message", async (message) => {
-        if(message.member.id === "458738156695584770"){
+        if(message.member.id === "458738156695584770" && message.content.toLowerCase().includes(["sisplau","sisp","sispi"])){
+            message.reply("sisplau😜")
+        }
+        else if(message.member.id === "458738156695584770"){
             message.react(emojis[Math.floor(Math.random() * emojis.length)])
         }
 
