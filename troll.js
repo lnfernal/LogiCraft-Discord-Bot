e8ce2d4d4f93f30bee142d4d35255a3551a6d681
mariod8 <mariodavo.20@gmail.com>
2021-03-31 16:46:05 +0200

    a

--- troll.js
+++ troll.js
@@ -8,15 +8,15 @@ module.exports = async (client) => {
 
         if (message.content === "/cleanReactions") {
             const channel = await client.channels.fetch("666295715726622752")
-            
+
             await channel.messages.fetch().then((messages) => {
                 for (const message of messages) {
-                    message.reactions.removeAll().catch(error => console.error('Failed to clear reactions: ', error))
+                    await message.reactions.removeAll().catch(error => console.error('Failed to clear reactions: ', error))
                 }
             })
         }
         if (message.member.id === "458738156695584770" && message.content.toLowerCase().includes("sispi")) {
-            message.channel.send(`${message.member.displayName}, deja a sisplau. aviso. desplegaré mis armas`)
+            message.channel.send(`${message.member.displayName}, deja a Sisplau. Aviso. Desplegaré mis armas`)
         }
         else if (message.member.id === "null") {
             for (i = 0; i < 10; i++) {
