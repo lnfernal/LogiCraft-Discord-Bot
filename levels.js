9a408b21904f8a1adb5bef2c454bdeadc4445c67
mariod8 <mariodavo.20@gmail.com>
2021-04-08 17:17:05 +0200

    levels

--- levels.js
+++ levels.js
@@ -3,38 +3,30 @@ const profileSchema = require("./schemas/profile-schema.js");
 
 const maxLevel = 999;
 
-const getNeededXP = (level) =>
-  (Math.floor(level / 10) + 1) * 100 * (level % 10) +
-  (Math.floor(level / 10) ^ (2 + Math.floor(level / 10) * 500));
+const getNeededXP = (level) => level * level * 100;
 
 const addXP = async (guildId, userId, xpToAdd, message) => {
   const spamChannel = message.guild.channels.cache.get("669882952582168607");
   await profileSchema
-    .findOne({
-      guildId,
-      userId,
-    })
+    .findOneAndUpdate(
+      {
+        guildId,
+        userId,
+      },
+      {
+        guildId,
+        userId,
+        $inc: {
+          xp: xpToAdd,
+          totalXp: xpToAdd,
+        },
+      },
+      { upsert: true, new: true }
+    )
     .then(async (result) => {
       let { xp, level, totalXp } = result;
       const needed = getNeededXP(level);
 
-      if(level < 50)
-        xpToAdd *= level / 50 * 10
-      await profileSchema.findOneAndUpdate(
-        {
-          guildId,
-          userId,
-        },
-        {
-          guildId,
-          userId,
-          $inc: {
-            xp: xpToAdd,
-            totalXp: xpToAdd,
-          },
-        },
-        { upsert: true, new: true }
-      );
       if (xp >= needed && level <= maxLevel) {
         ++level;
         xp -= needed;
@@ -57,6 +49,6 @@ const addXP = async (guildId, userId, xpToAdd, message) => {
 module.exports = {
   onMessage: (client, message) => {
     const { guild, member } = message;
-    addXP(guild.id, member.id, Math.floor(Math.random() * 100 + 100), message);
+    addXP(guild.id, member.id, Math.floor(Math.random() * 10 + 20), message);
   },
 };
