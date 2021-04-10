a745ae5b8d979f49e1fe0dd59222721410d3c533
mariod8 <mariodavo.20@gmail.com>
2021-04-10 16:36:21 +0200

    char ocurrencies fixed

--- commands/levels/xprank.js
+++ commands/levels/xprank.js
@@ -30,7 +30,7 @@ var getNames = (users) => {
   var names = ``;
 
   for (i = 0; i < users.length; i++) names += `${i + 1}. ${users[i].name}\n`;
-  return names.replace("_", "—");
+  return names.replace("_", "");
 };
 
 var getXP = (users) => {
