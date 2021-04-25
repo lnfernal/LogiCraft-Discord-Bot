6a5ff763dc589d5fc3593d34bb98ae436c03a8ee
mariod8 <mariodavo.20@gmail.com>
2021-04-25 10:21:43 +0200

    fix

--- misc/user-activity.js
+++ misc/user-activity.js
@@ -2,7 +2,7 @@ let activityUsersChannel;
 
 module.exports = {
   init: (guild) => {
-    activityUsersChannel = guild.channels.cache.get("834506309242978345");
+    activityUsersChannel = guild.channels.cache.get("804646865621614593");
   },
   onJoin: (member) => {
     activityUsersChannel.send(`🟢 **${member.user.username}** se ha unido!`);
