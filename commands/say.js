8bc7a9b69f1e6a2bda30df992d04e334cc8dd340
Mario <66087392+mariod8@users.noreply.github.com>
2021-04-30 16:51:33 +0200

    spam fix

--- commands/say.js
+++ commands/say.js
@@ -5,9 +5,6 @@ module.exports = {
   maxArgs: 199,
   cooldown: 5,
   callback: (message, arguments, text, client) => {
-    const matches = text.match(/^<@!?(\d+)>$/)
-            if(matches)
-              matches.forEach(match => {text.replace(match,"")})
-    message.channel.send(text);
+    message.channel.send(text.replace(/^<@!?(\d+)>$/,""));
   },
 };
