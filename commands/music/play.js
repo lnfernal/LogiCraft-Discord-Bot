d40c1334c4aad869da41b00a02c33db515ebcbed
Mario <66087392+mariod8@users.noreply.github.com>
2021-04-30 18:20:39 +0200

    fix

--- commands/music/play.js
+++ commands/music/play.js
@@ -5,10 +5,10 @@ module.exports = {
   maxArgs: 10,
   callback: async (message, arguments, text, client) => {
     const { channel } = message.member.voice;
-    const emojis = await require("../../utils/emojis").discEmojis(client);
+    const emojis = await require("../../utils/emojis.js").discEmojis(client);
     if (!channel)
       return message.channel.send(
-        `**${message.member.displayName}**, necesitas estar en un canal de voz`
+        `**${message.member.displayName}**, necesitas estar en un canal de voz`;
       );
     let audio = await client.player.play(message, text);
     if (audio) message.channel.send(`${emojis[Math.random() * emojis.length]} Reproduciendo **${audio.name}**`);
