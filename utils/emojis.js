244cc43f29aa61740a0f6569e202c62d84cf6013
Mario <66087392+mariod8@users.noreply.github.com>
2021-04-30 18:09:28 +0200

    disc emojis

--- utils/emojis.js
+++ utils/emojis.js
@@ -19,3 +19,15 @@ module.exports.logibotEmojis = async (client) => {
   });
   return emojis;
 };
+
+module.exports.discEmojis = async (client) => {
+  let emojis = {};
+
+  const guild = client.guilds.cache.get("829448956417015828");
+
+  guild.emojis.cache.forEach((emoji) => {
+    if(emoji.name.includes("musicDisc"))
+    emojis[`${emoji.name}`] = emoji;
+  });
+  return emojis;
+};
