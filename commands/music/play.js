6fdcaa51a31be49063753de35fca2c886c0807b2
Mario <66087392+mariod8@users.noreply.github.com>
2021-04-30 18:09:20 +0200

    disc emojis

--- commands/music/play.js
+++ commands/music/play.js
@@ -5,11 +5,12 @@ module.exports = {
   maxArgs: 10,
   callback: async (message, arguments, text, client) => {
     const { channel } = message.member.voice;
+    const emojis = await require("../../utils/emojis").discEmojis(client);
     if (!channel)
       return message.channel.send(
         `**${message.member.displayName}**, necesitas estar en un canal de voz`
       );
     let audio = await client.player.play(message, text);
-    if (audio) message.channel.send(`Reproduciendo **${audio.name}**`);
+    if (audio) message.channel.send(`${emojis[Math.random() * emojis.length]} Reproduciendo **${audio.name}**`);
   },
 };
