c6acc5c43e45f0ea432e470481bb6f984276d79a
Mario <66087392+mariod8@users.noreply.github.com>
2021-05-26 17:55:54 +0200

    cancel

--- commands/cancel.js
+++ commands/cancel.js
@@ -1,6 +1,9 @@
 module.exports = {
  commands: "cancel",
+ permissions: "ADMINISTRATOR",
+ permissionError = "Solo un Administrador puede cancelar el reset de un server",
  callback: async (message, arguments, text, client) => {
    require("./troll.js").cancel()
+  message.channel.send("Cancelado")
  }
 }
