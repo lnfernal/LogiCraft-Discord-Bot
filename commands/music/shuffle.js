ff45c007829968f5689a54950e4c14c996ccc9c8
mariod8 <mariodavo.20@gmail.com>
2021-06-04 15:43:14 +0200

    Offline

--- commands/music/shuffle.js
+++ commands/music/shuffle.js
@@ -3,7 +3,7 @@ module.exports = {
   callback: async (message, args, text, client) => {
     try {
       let shuffle = client.player.shuffle(message)
-      if (shuffle) message.channel.send(`La cola está en aleatorio`)
+      if (shuffle) message.channel.send(`La cola se ha aleatorizado`)
     } catch (e) {
       message.channel.send(`No hay ninguna cola en curso`)
     }
