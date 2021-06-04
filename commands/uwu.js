e777746b8016f6075fbc399adea5940131714184
mariod8 <mariodavo.20@gmail.com>
2021-06-04 16:36:02 +0200

    Typo

--- commands/uwu.js
+++ commands/uwu.js
@@ -2,6 +2,6 @@ module.exports = {
   commands: "uwu",
   callback: async (message, args, text, client) => {
     const mute = require("./moderation/mute")
-    mute.callback(message, [`${message.author.username}`, "30m", "Demasiado UwU"], "", client)
+    await mute.callback(message, [`${message.author.username}`, "30m", "Demasiado UwU"], "", client)
   },
 }
