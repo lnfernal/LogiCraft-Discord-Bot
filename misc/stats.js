da64f872b6c1699954908118c2e8fce670141b5c
mariod8 <mariodavo.20@gmail.com>
2021-06-05 11:20:57 +0200

    Typo

--- misc/stats.js
+++ misc/stats.js
@@ -6,7 +6,7 @@ module.exports.inc = async message => {
 
   await userUtils.incUserSchema(guild, author, "messages", 1)
   await userUtils.incUserSchema(guild, author, "words", message.content.split(" ").length)
-  if (message.attachments.size() > 0) await userUtils.incUserSchema(guild, author, "files", 1)
+  if (message.attachments.length > 0) await userUtils.incUserSchema(guild, author, "files", 1)
   if (/<a?:.+:\d+>|:.+:/.test(content))
     await userUtils.incUserSchema(guild, author, "emojis", /<a?:.+:\d+>|:.+:/.match(content).length)
   if (message.reference) await userUtils.incUserSchema(guild, author, "replies", 1)
