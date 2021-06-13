36dc72f22e66227e2c2ce3c18abc1725c5a40af5
Mario <66087392+mariod8@users.noreply.github.com>
2021-06-13 11:49:57 +0200

    Ping

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,33 @@ module.exports = {
   commands: ["ping", "latency"],
   expectedArgs: "<content>",
   callback: (message, args, text, client) => {
-    message.channel.send(`⏳**${client.ws.ping}ms**`)
+    const emojis = require("@emojis").logibotEmojis(client)
+    const moves = [`${emojis.little}`, `${emojis.moderate}`,`${emojis.high}`],
+      { channel, member } = message,
+      changeDelay = 1700,
+      uptime = 10,
+      changeMoves = async () => {
+        const currentMoveIndex = moves.indexOf(danceMessage.content)
+        const msgExists = await channel.messages.fetch(danceMessage.id).catch(console.error)
+
+        if (!ended && msgExists)
+          setTimeout(() => {
+            changeMoves()
+          }, changeDelay)
+        await danceMessage.edit(currentMoveIndex == moves.length - 1 ? moves[0].concat(` **${client.ws.ping}ms**`) : moves[currentMoveIndex + 1].concat(` **${client.ws.ping}ms**`))
+      }
+
+    let ended = false,
+      danceMessage
+
+    await channel.send(moves[Math.floor(Math.random() * moves.length)].concat(` **${client.ws.ping}ms**`)).then(msg => {
+      danceMessage = msg
+    })
+    setTimeout(() => {
+      changeMoves()
+    }, changeDelay)
+    setTimeout(() => {
+      ended = true
+    }, uptime * 1000)
   },
 }
