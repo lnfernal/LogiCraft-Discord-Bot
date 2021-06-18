ea89c8ee8149027275b4e48ae1bf2866021cf573
Mario <66087392+mariod8@users.noreply.github.com>
2021-06-19 00:08:10 +0200

    Avatar

--- commands/levels/xp.js
+++ commands/levels/xp.js
@@ -35,7 +35,7 @@ const xpEmbed = async (message, target, profile, needed) => {
         totalXp: s.formatNumber(totalXp),
       })
     )
-    .setThumbnail(target.avatarURL({ dynamic: true, size: 128 })/*await userUtils.getUserAvatar(target)*/)
+    .setThumbnail(await userUtils.getUserAvatar(target))
     .setFooter(progressMade() + ` ${Math.round(((xp / needed) * 1000) / 10)}%`)
   message.channel.send(embed)
 }
