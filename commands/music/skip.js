1d28b56543f9b33f1544a8beaef9a7e0c84f6188
mariod8 <mariodavo.20@gmail.com>
2021-06-21 12:19:02 +0200

    Multiple Skips

--- commands/music/skip.js
+++ commands/music/skip.js
@@ -1,6 +1,14 @@
 module.exports = {
   commands: "skip",
+  expectedArgs: "[songs]",
+  maxArgs: 1,
   callback: async (message, args, text, client) => {
+    let songs
+    if (args)
+      if (isNaN(args[0])) return
+      else songs = args[0]
+    else songs = 1
+
     try {
       await client.player.skip(message)
       setTimeout(() => {
